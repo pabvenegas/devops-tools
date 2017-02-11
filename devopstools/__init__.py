7c13198dbc96561a5c377dc5e1ec1a03286e21b8
Pablo Venegas <pablo.venegas@gmail.com>
2017-02-11 12:07:18 +1100

    Initial commit

---
+++ devopstools/__init__.py
@@ -0,0 +1,22 @@
+'''
+devops-tools: Main module
+
+Copyright 2017, Pablo Venegas
+Licensed under MIT.
+'''
+
+
+def main():
+    '''
+    Main function of the boilerplate code is the entry point of the 'devopstools' executable script (defined in setup.py).
+    
+    Use doctests, those are very helpful.
+    
+    >>> main()
+    Hello
+    >>> 2 + 2
+    4
+    '''
+    
+    print("Hello")
+
